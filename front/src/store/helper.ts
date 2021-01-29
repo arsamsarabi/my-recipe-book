a819238dc1c66ce3cb1b0654f1ab530e35dd4f9d
Lee <leevincentwilsom@gmail.com>
2021-01-29 11:49:34 +0000

    more es lint

--- front/src/store/helper.ts
+++ front/src/store/helper.ts
@@ -1,6 +1,7 @@
 import { useState } from '@hookstate/core'
 import {StateMethods, StateMethodsDestroy} from '@hookstate/core/dist'
 
+// eslint-disable-next-line no-unused-vars
 export const useGlobalState =<T> (store:  StateMethods<T> & StateMethodsDestroy): [T,(newValue:T)=>void] => {
   const state = useState(store)
 
