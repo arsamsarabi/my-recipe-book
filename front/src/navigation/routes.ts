4863813e2524b9d704a8290d4826b54e33d8b191
Lee <leevincentwilson@gmail.com>
2021-01-31 11:23:10 +0000

    fixed typescript no vars issue

--- front/src/navigation/routes.ts
+++ front/src/navigation/routes.ts
@@ -1,10 +1,6 @@
 export enum routes  {
-  // eslint-disable-next-line no-unused-vars
   DASHBOARD = '/dashboard',
-  // eslint-disable-next-line no-unused-vars
   INGREDIENTS = '/ingredients',
-  // eslint-disable-next-line no-unused-vars
   ADD_INGREDIENT = '/add-ingredient',
-  // eslint-disable-next-line no-unused-vars
   REGISTER = '/register',
 }
