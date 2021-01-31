d26e9f4b9fa16e708bcc61a7a9d1513515c98fe6
Lee <leevincentwilson@gmail.com>
2021-01-31 11:25:32 +0000

    1

--- back/app/middlewares/auth.ts
+++ back/app/middlewares/auth.ts
@@ -2,13 +2,11 @@ import jwt from 'jsonwebtoken'
 import config from 'config'
 
 export const authMiddleware = (req: any, res: any, next: any) => {
-  debugger;
   const token = req.header(config.get('const.authHeader'))
   if (!token) return res.status(401).send('Access denied. Token not found')
 
   try {
-    const decoded = jwt.verify(token, config.get('jwtPrivateKey'))
-    req.user = decoded
+    req.user = jwt.verify(token, config.get('jwtPrivateKey'))
     next()
   } catch (error) {
     return res.status(400).send('Invalid token.')
